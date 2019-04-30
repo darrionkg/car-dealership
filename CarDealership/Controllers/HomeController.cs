1c8b0ca83b8842f6b40a3a7a407d147cf13dce0c
Darrion Gering and Heather Zurek <heather@>
2019-04-30 11:42:37 -0700

    Add basic files and classes

---
+++ CarDealership/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using CarDealership.Models;
+using System.Collections.Generic;
+
+
+namespace CarDealership.Controllers
+{
+  public class HomeController : Controller
+  {
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      List<Car> allCars = Car.GetAll();
+      return View(allCars);
+    }
+
+    [HttpGet("/cars/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+
+
+  }
+}
---
+++ CarDealership/Models/Cars.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace CarDealership.Models {
+
+  class Car
+  {
+    private string MakeModel;
+    private int Price;
+    private int Miles;
+    private static List<Car> _instances = new List<Car> {};
+
+    public Car(string makeModel, int price, int miles)
+    {
+      MakeModel = makeModel;
+      Price = price;
+      Miles = miles;
+      _instances.Add(this);
+    }
+
+    public string GetMakeModel()
+    {
+      return MakeModel;
+    }
+
+    public int GetPrice()
+    {
+      return Price;
+    }
+
+    public int GetMiles()
+    {
+      return Miles;
+    }
+
+    public static List<Car> GetAll()
+    {
+      return _instances;
+    }
+
+    public void SetPrice(int newPrice)
+    {
+      Price = newPrice;
+    }
+
+    public bool WorthBuying(int maxPrice)
+    {
+      return (Price < maxPrice);
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+  }
+
+}
